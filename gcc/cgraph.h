8ca57badae943c59ad9fcd3f27148945410b7712
Max Kellermann <max.kellermann@gmail.com>
2017-03-27 12:27:19 +0200

    gcc/cgraph: fix copy'n'paste oversight in in_same_comdat_group_p()
    This has never worked, but fortunately the "inlined_to" check was
    never really necessary.

--- gcc/cgraph.h
+++ gcc/cgraph.h
@@ -2376,7 +2376,7 @@ symtab_node::in_same_comdat_group_p (symtab_node *target)
 {
   symtab_node *source = this;
 
-  if (cgraph_node *cn = dyn_cast <cgraph_node *> (target))
+  if (cgraph_node *cn = dyn_cast <cgraph_node *> (source))
     {
       if (cn->global.inlined_to)
 	source = cn->global.inlined_to;
